021e1410b69c9dbe2b4deda88da8077187fbb3f8
khushii-hub <72705845+khushii-hub@users.noreply.github.com>
2024-08-29 19:28:02 +0530

    Add files via upload

---
+++ trying.java
@@ -0,0 +1,14 @@
+class P1{
+    public static void main(String args[])
+    {
+       // int n;
+        //Scanner sc = int S
+        for(int i =0; i <4;i++)
+        {
+            for(int j =0; j<4;j++){
+                if( i >= j){
+            System.out.print("*");
+                }}System.out.println("\n");
+        }
+    }
+}
